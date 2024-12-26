f1fa30351d4f0b5cfa7c168570972f9eade21d66
OBESTMAN <oerebestman@yahoo.com>
2024-12-26 17:19:49 +0000

    implemented single paint

--- lib/src/_image_painter.dart
+++ lib/src/_image_painter.dart
@@ -22,6 +22,8 @@ class DrawImage extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
+    final _offset = _controller.offsets;
+
     ///paints [ui.Image] on the canvas for reference to draw over it.
     paintImage(
       canvas: canvas,
@@ -34,85 +36,52 @@ class DrawImage extends CustomPainter {
     );
 
     ///paints all the previoud paintInfo history recorded on [PaintHistory]
-    for (final item in _controller.paintHistory) {
-      final _offset = item.offsets;
-      final _painter = item.paint;
-      switch (item.mode) {
-        case PaintMode.rect:
-          canvas.drawRect(Rect.fromPoints(_offset[0]!, _offset[1]!), _painter);
-          break;
-        case PaintMode.line:
-          canvas.drawLine(_offset[0]!, _offset[1]!, _painter);
-          break;
-        case PaintMode.circle:
-          final path = Path();
-          path.addOval(
-            Rect.fromCircle(
-                center: _offset[1]!,
-                radius: (_offset[0]! - _offset[1]!).distance),
-          );
-          canvas.drawPath(path, _painter);
-          break;
-        case PaintMode.arrow:
-          drawArrow(canvas, _offset[0]!, _offset[1]!, _painter);
-          break;
-        case PaintMode.dashLine:
-          final path = Path()
-            ..moveTo(_offset[0]!.dx, _offset[0]!.dy)
-            ..lineTo(_offset[1]!.dx, _offset[1]!.dy);
-          canvas.drawPath(_dashPath(path, _painter.strokeWidth), _painter);
-          break;
-        case PaintMode.freeStyle:
-            // ..strokeCap = StrokeCap.round
-            // ..style = PaintingStyle.stroke;
-          for (int i = 0; i < _offset.length - 1; i++) {
-            if (_offset[i] != null && _offset[i + 1] != null) {
-              canvas.drawLine(_offset[i]!, _offset[i + 1]!, _painter);
-            }
+    //final _offset = item.offsets;
+    final _painter = _controller.paintHistory[0].paint;
+    switch (_controller.mode) {
+      case PaintMode.rect:
+        canvas.drawRect(Rect.fromPoints(_offset[0]!, _offset[1]!), _painter);
+        break;
+      case PaintMode.line:
+        canvas.drawLine(_offset[0]!, _offset[1]!, _painter);
+        break;
+      case PaintMode.circle:
+        final path = Path();
+        path.addOval(
+          Rect.fromCircle(
+              center: _offset[1]!,
+              radius: (_offset[0]! - _offset[1]!).distance),
+        );
+        canvas.drawPath(path, _painter);
+        break;
+      case PaintMode.arrow:
+        drawArrow(canvas, _offset[0]!, _offset[1]!, _painter);
+        break;
+      case PaintMode.dashLine:
+        final path = Path()
+          ..moveTo(_offset[0]!.dx, _offset[0]!.dy)
+          ..lineTo(_offset[1]!.dx, _offset[1]!.dy);
+        canvas.drawPath(_dashPath(path, _painter.strokeWidth), _painter);
+        break;
+      case PaintMode.freeStyle:
+        final newPaint = Paint()
+          ..color = Colors.blue.withValues(alpha: 0.2)
+          ..strokeWidth = 5
+          ..strokeCap = StrokeCap.round
+          ..style = PaintingStyle.stroke;
+
+        for (int i = 0; i < _offset.length - 1; i++) {
+          if (_offset[i] != null && _offset[i + 1] != null) {
+            final path = Path()
+              ..moveTo(_offset[i]!.dx, _offset[i]!.dy)
+              ..lineTo(_offset[i + 1]!.dx, _offset[i + 1]!.dy);
+
+            // Use the consistent 'newPaint' object
+            canvas.drawPath(path, newPaint);
           }
-          // for (int i = 0; i < _offset.length - 1; i++) {
-          //   if (_offset[i] != null && _offset[i + 1] != null) {
-          //     // Only draw line if points are close enough
-          //     double distance = (_offset[i]! - _offset[i + 1]!).distance;
-          //     if (distance < 50) { // Adjust threshold as needed
-          //       final path = Path()
-          //         ..moveTo(_offset[i]!.dx, _offset[i]!.dy)
-          //         ..lineTo(_offset[i + 1]!.dx, _offset[i + 1]!.dy);
-          //       canvas.drawPath(path, _painter..strokeCap = StrokeCap.round);
-          //     } else {
-          //       // Draw individual points if too far apart
-          //       canvas.drawPoints(PointMode.points, [_offset[i]!], _painter..strokeCap = StrokeCap.round);
-          //       if (i == offset.length - 2) {
-          //         canvas.drawPoints(PointMode.points, [_offset[i + 1]!], _painter..strokeCap = StrokeCap.round);
-          //       }
-          //     }
-          //   }
-          // }
-          break;
-        case PaintMode.text:
-          final textSpan = TextSpan(
-            text: item.text,
-            style: TextStyle(
-              color: _painter.color,
-              fontSize: 6 * _painter.strokeWidth,
-              fontWeight: FontWeight.bold,
-            ),
-          );
-          final textPainter = TextPainter(
-            text: textSpan,
-            textAlign: TextAlign.center,
-            textDirection: TextDirection.ltr,
-          );
-          textPainter.layout(minWidth: 0, maxWidth: size.width);
-          final textOffset = _offset.isEmpty
-              ? Offset(size.width / 2 - textPainter.width / 2,
-                  size.height / 2 - textPainter.height / 2)
-              : Offset(_offset[0]!.dx - textPainter.width / 2,
-                  _offset[0]!.dy - textPainter.height / 2);
-          textPainter.paint(canvas, textOffset);
-          break;
-        default:
-      }
+        }
+        break;
+      default:
     }
 
     ///Draws ongoing action on the canvas while indrag.
@@ -143,30 +112,22 @@ class DrawImage extends CustomPainter {
           canvas.drawPath(_dashPath(path, _paint.strokeWidth), _paint);
           break;
         case PaintMode.freeStyle:
-          final _offset = _controller.offsets;
+          final newPaint = Paint()
+            ..color = Colors.blue.withValues(alpha: 0.2)
+            ..strokeWidth = 5
+            ..strokeCap = StrokeCap.round
+            ..style = PaintingStyle.stroke;
+
           for (int i = 0; i < _offset.length - 1; i++) {
             if (_offset[i] != null && _offset[i + 1] != null) {
-              canvas.drawLine(_offset[i]!, _offset[i + 1]!, _paint);
+              final path = Path()
+                ..moveTo(_offset[i]!.dx, _offset[i]!.dy)
+                ..lineTo(_offset[i + 1]!.dx, _offset[i + 1]!.dy);
+
+              // Use the consistent 'newPaint' object
+              canvas.drawPath(path, newPaint);
             }
           }
-
-          // for (int i = 0; i < points.length - 1; i++) {
-          //   if (points[i] != null && points[i + 1] != null) {
-          //     double distance = (points[i]! - points[i + 1]!).distance;
-          //     if (distance < 50) {
-          //       canvas.drawLine(
-          //           Offset(points[i]!.dx, points[i]!.dy),
-          //           Offset(points[i + 1]!.dx, points[i + 1]!.dy),
-          //           _paint..strokeCap = StrokeCap.round);
-          //     } else {
-          //       canvas.drawPoints(PointMode.points,
-          //           [Offset(points[i]!.dx, points[i]!.dy)], _paint);
-          //     }
-          //   } else if (points[i] != null && points[i + 1] == null) {
-          //     canvas.drawPoints(PointMode.points,
-          //         [Offset(points[i]!.dx, points[i]!.dy)], _paint);
-          //   }
-          // }
           break;
         default:
       }
